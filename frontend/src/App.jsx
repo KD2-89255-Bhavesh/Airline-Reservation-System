b2c7efda36dcc80dba4505c70bd4f761c2013693
bhavesh <bhaveshgupta13524@gmail.com>
2025-07-22 09:38:29 +0530

    some chnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import CustomerFeedback from "./pages/feedback/CustomerFeedback";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/feedback" element={<CustomerFeedback/>} />
       </Routes>
     </>
   );
